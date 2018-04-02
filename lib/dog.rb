268b16435b38d5374bd15b1997c48d3d92ce8bd6
Kelsey Fecho <kfecho30@gmail.com>
2018-04-02 13:31:33 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,10 +2,10 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
 
-  def initialize(name:, breed:, id=nil)
+  def initialize(name:, breed:)
     @name = name
     @breed = breed
-    @id = id 
+    @id = id
   end
 
 end
