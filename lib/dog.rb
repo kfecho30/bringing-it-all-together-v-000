8638b2445390723685bdea65b26ef3592816b08c
Kelsey Fecho <kfecho30@gmail.com>
2018-04-02 14:04:23 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -33,9 +33,9 @@ class Dog
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
     if !dog.empty?
       dog_data = dog[0]
-      self.new_from_db[dog_data]
+      dog = self.new_from_db[dog_data]
     else
-      self.create(name: name, breed: breed)
+      dog = self.create(name: name, breed: breed)
     end
     dog
   end
