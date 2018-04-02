75e7c9fe7aea944be7d76b255da438196ce3b90c
Kelsey Fecho <kfecho30@gmail.com>
2018-04-02 13:37:23 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,7 +11,7 @@ class Dog
   def create(name, breed)
     dog = Dog.new(name, breed)
     DB[:conn].execute("INSERT INTO dogs (name, breed) VALUES (?, ?)", name, breed)
-
+    @id = DB[:conn].execute("SELECT last_insert_rowid FROM dogs;")
   end
 
 
